85252a2646d7255906b991130198f16a80eead0f
Ivo Flipse <ivo.flipse@zimmerbiomet.com>
2018-01-14 13:12:56 +0100

    Fixed test

--- factors/tests/test_factors.py
+++ factors/tests/test_factors.py
@@ -4,6 +4,6 @@ from factors import LifeTable
 def test_data():
     tables = LifeTable("AEG2011")
     testdata = tables.run_test()
-    assert testdata
+    assert testdata is not None
     error_squared = sum(testdata['difference'] * testdata['difference'])
     assert error_squared < 3.5e-5
