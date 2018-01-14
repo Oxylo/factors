c24dc783137872e09c7f395fe05b7d9f51e3a659
Ivo Flipse <ivo.flipse@zimmerbiomet.com>
2018-01-14 15:22:55 +0100

    Adding the NewLifeTable so we can test that it works Adding tests to verify that the new code still works

--- factors/__init__.py
+++ factors/__init__.py
@@ -1,6 +1,7 @@
-from .models import LifeTable  # NOQA
+from .models import LifeTable, NewLifeTable  # NOQA
 
+__version__ = "0.1.0"
 
 __all__ = [
-    'LifeTable',
+    'LifeTable', 'NewLifeTable', __version__
 ]
